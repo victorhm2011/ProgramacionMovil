10901d585f4888de153e8a561c0ea68ab2d5c749
Victor Hugo Montano <victor.montano@ucb.edu.bo>
2022-05-18 22:42:33 -0400

    [quinta]: Add fifth practice

---
+++ primera/app/src/main/java/edu/bo/ucb/Borrar.kt
@@ -0,0 +1,11 @@
+package edu.bo.ucb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Borrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_borrar)
+    }
+}
--- primera/app/src/main/java/edu/bo/ucb/ServiciosActivity.kt
+++ primera/app/src/main/java/edu/bo/ucb/ServiciosActivity.kt
@@ -2,10 +2,24 @@ package edu.bo.ucb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class ServiciosActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_servicios)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
     }
+
+
+    fun selectedButton(view: View) {
+        if ( view.id === R.id.btnServices) {
+            Toast.makeText( view.context, "Opción de Servicios", Toast.LENGTH_LONG).show()
+        }
+        Toast.makeText( view.context, "Evento no definido", Toast.LENGTH_LONG).show()
+    }
+
 }
