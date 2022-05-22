f5e293257d510de7dbf6a002967fd8c88021f1e4
Victor Hugo Montano <victor.montano@ucb.edu.bo>
2022-05-22 12:18:19 -0400

    Add fifth guide

--- app/src/main/java/edu/bo/quinta/MainActivity.kt
+++ app/src/main/java/edu/bo/quinta/MainActivity.kt
@@ -7,9 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportActionBar.let {
-            it?.setDisplayHomeAsUpEnabled(true)
-            it?.setDisplayShowHomeEnabled(true)}
     }
 }
