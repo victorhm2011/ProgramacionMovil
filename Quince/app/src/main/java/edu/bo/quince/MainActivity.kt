414348f5d1c8fdb0721268e574f3e9a318f424e9
Victor Hugo Montano <victor.montano@ucb.edu.bo>
2022-05-29 19:12:50 -0400

    Add fifteenth practice

---
+++ Quince/app/src/main/java/edu/bo/quince/MainActivity.kt
@@ -0,0 +1,32 @@
+package edu.bo.quince
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button3: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button3 = findViewById(R.id.btnSC)
+        button3.setOnClickListener {
+            if(isConexion(applicationContext)) {
+                Toast.makeText(this, "Tiene acceso a internet", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No tiene acceso a internet", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun isConexion( context: Context) : Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
+}
