5f8382eafbebb38e43c6472dbd480ba2ee84a4d5
Victor Hugo Montano <victor.montano@ucb.edu.bo>
2022-05-29 20:45:03 -0400

    Add sixteenth practice

--- Quince/app/src/main/java/edu/bo/quince/MainActivity.kt
+++ Quince/app/src/main/java/edu/bo/quince/MainActivity.kt
@@ -1,15 +1,20 @@
 package edu.bo.quince
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     lateinit var button3: Button
+    lateinit var myImageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +27,12 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "No tiene acceso a internet", Toast.LENGTH_LONG).show()
             }
         }
+
+        myImageView = findViewById(R.id.my_image_view)
+        val picasso = Picasso.get()
+        picasso.load(
+            "https://i.pinimg.com/564x/a8/6e/26/a86e26dffbcd0f8ffd0b7a6a4809ec68.jpg")
+            .into(myImageView)
     }
 
     fun isConexion( context: Context) : Boolean {
